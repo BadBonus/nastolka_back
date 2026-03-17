e1557b5ac36e4993e4e7910b1dc0523643778920
ArturYes <pikavitik@gmail.com>
2026-03-17 23:53:25 +0300

    FEAT: обновлены корсы с localhost:3000

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Nastolka API')
     .setDescription('Документация бэкенда')
